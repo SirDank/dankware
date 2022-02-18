684e1756bdc7ac5f3cbf76f9f2198344ac6c30e5
SirDankenstein <52797753+SirDank@users.noreply.github.com>
2022-02-18 14:38:14 +0530

    1.10

--- dankware/__init__.py
+++ dankware/__init__.py
@@ -1,11 +1,10 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from alive_progress import alive_bar
-from colorama import init, Fore, Style
+from colorama import Fore, Style
 import random
 import time
 import os
 
-init(autoreset=True)
 white = Fore.WHITE + Style.BRIGHT
 magenta = Fore.MAGENTA + Style.BRIGHT
 red = Fore.RED + Style.BRIGHT
@@ -36,7 +35,7 @@ def multithread(function, threads: int = 1, input_one = None, input_two = None, 
     elif not one_isNone and not two_isNone:
 
         if one_isList and two_isList:
-            if len(input_one) != len(input_two):print(f"\n  \x1b[1;37;40m> \x1b[1;31;40mMULTITHREAD ERROR\x1b[1;37;40m! - \x1b[1;31;40minput_one\x1b[1;37;40m[\x1b[1;31;40m{len(input_one)}\x1b[1;37;40m] \x1b[1;31;40mand input_two\x1b[1;37;40m[\x1b[1;31;40m{len(input_two)}\x1b[1;37;40m] \x1b[1;31;40mdo not have the same length\x1b[1;37;40m!");return
+            if len(input_one) != len(input_two):print(clr(f"\n  > MULTITHREAD ERROR! - input_one[{len(input_one)}] and input_two[{len(input_two)}] do not have the same length!",2) + Style.RESET_ALL);return
             for index in range(len(input_one)):futures.append(executor.submit(function, input_one[index], input_two[index]))
             
         elif one_isList:
@@ -63,32 +62,32 @@ def cls() -> None:
     else:_ = os.system('clear')
 
 def clr(text: str, mode: int = 1) -> str: # colour chars with magenta and white / red and white
-    
+
     if mode == 1:
         text = str(text).replace("[",f"{magenta}[{white}").replace("]",f"{magenta}]{white}")
         for char in ['>','.',',','=','-','!','|','(',')','/',':']:text = text.replace(char, f"{magenta}{char}{white}")
         for bool in ['true', 'True', 'TRUE']:text = text.replace(bool, f"{green}{bool}{white}")
         for bool in ['false', 'False', 'FALSE']:text = text.replace(bool, f"{red}{bool}{white}")
-        return f"{white}{text}"
+        return f"{white}{text}" + Style.RESET_ALL
     
     elif mode == 2:
         text = str(text).replace("[",f"{white}[{red}").replace("]",f"{white}]{red}")
         for char in ['>','.',',','=','-','!','|','(',')','/',':']:text = text.replace(char, f"{white}{char}{red}")
         for bool in ['true', 'True', 'TRUE']:text = text.replace(bool, f"{green}{bool}{red}")
-        return f"{red}{text}"
+        return f"{red}{text}" + Style.RESET_ALL
     
-    else:print(f"\n  {white}> {red}CLR ERROR{white}! - {red}Wrong mode {white}[{red}{mode}{white}]")
+    else:return f"\n  {white}> {red}CLR ERROR{white}! - {red}Wrong mode {white}[{red}{mode}{white}]" + Style.RESET_ALL
 
 def clr_banner(banner: str) -> str: # randomized banner color
-    
+
     bad_colors = ['BLACK', 'WHITE', 'LIGHTBLACK_EX', 'LIGHTWHITE_EX', 'RESET']
     codes = vars(Fore)
     colors = [codes[color] for color in codes if color not in bad_colors]
     colored_chars = [random.choice(colors) + char for char in banner]
-    return ''.join(colored_chars)
+    return ''.join(colored_chars) + Style.RESET_ALL
 
 def align_banner(banner: str, coloured_banner: str = "") -> str: # center align banner with terminal size ( supports coloured and non-coloured )
-    
+
     width = os.get_terminal_size().columns
     banner = banner.splitlines()
     if coloured_banner == "":coloured = False
@@ -96,143 +95,153 @@ def align_banner(banner: str, coloured_banner: str = "") -> str: # center align 
     for i in range(len(banner)):
         if coloured:banner[i] = banner[i].center(width).replace(banner[i],coloured_banner[i])
         else:banner[i] = banner[i].center(width)
-    return '\n'.join(banner)
+    return '\n'.join(banner) + Style.RESET_ALL
         
 def dankware_banner() -> None:
 
     banner="\n 8 888888888o.     \n 8 8888    `^888.  \n 8 8888        `88.\n 8 8888         `88\n 8 8888          88\n 8 8888          88\n 8 8888         ,88\n 8 8888        ,88'\n 8 8888    ,o88P'  \n 8 888888888P'     \n\n\n          .8.         \n         .888.        \n        :88888.       \n       . `88888.      \n      .8. `88888.     \n     .8`8. `88888.    \n    .8' `8. `88888.   \n   .8'   `8. `88888.  \n  .888888888. `88888. \n .8'       `8. `88888.\n\n\n b.             8\n 888o.          8\n Y88888o.       8\n .`Y888888o.    8\n 8o. `Y888888o. 8\n 8`Y8o. `Y88888o8\n 8   `Y8o. `Y8888\n 8      `Y8o. `Y8\n 8         `Y8o.`\n 8            `Yo\n\n\n 8 8888     ,88'\n 8 8888    ,88' \n 8 8888   ,88'  \n 8 8888  ,88'   \n 8 8888 ,88'    \n 8 8888 88'     \n 8 888888<      \n 8 8888 `Y8.    \n 8 8888   `Y8.  \n 8 8888     `Y8.\n\n\n `8.`888b                 ,8'\n  `8.`888b               ,8' \n   `8.`888b             ,8'  \n    `8.`888b     .b    ,8'   \n     `8.`888b    88b  ,8'    \n      `8.`888b .`888b,8'     \n       `8.`888b8.`8888'      \n        `8.`888`8.`88'       \n         `8.`8' `8,`'        \n          `8.`   `8'         \n\n\n          .8.         \n         .888.        \n        :88888.       \n       . `88888.      \n      .8. `88888.     \n     .8`8. `88888.    \n    .8' `8. `88888.   \n   .8'   `8. `88888.  \n  .888888888. `88888. \n .8'       `8. `88888.\n\n\n 8 888888888o.  \n 8 8888    `88. \n 8 8888     `88 \n 8 8888     ,88 \n 8 8888.   ,88' \n 8 888888888P'  \n 8 8888`8b      \n 8 8888 `8b.    \n 8 8888   `8b.  \n 8 8888     `88.\n\n\n 8 8888888888   \n 8 8888         \n 8 8888         \n 8 8888         \n 8 888888888888 \n 8 8888         \n 8 8888         \n 8 8888         \n 8 8888         \n 8 888888888888 \n "
     cls()
     print("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n")
-    for line in align_banner(banner, clr_banner(banner)).splitlines():
-        time.sleep(0.05);print(line)
-    for i in range(18):
-        time.sleep(0.1);print("\n")
+    for line in align_banner(banner, clr_banner(banner)).splitlines():time.sleep(0.05);print(line)
+    for i in range(18):time.sleep(0.1);print("\n")
     cls()
     
 def fade(text: str, colour: str = "purple") -> str: # credits to https://github.com/venaxyt/gratient & https://github.com/venaxyt/fade <3
-    
+
     colour = colour.lower()
     available_colours = ['black','red','green','cyan','blue','purple','random','black-v','red-v','green-v','cyan-v','blue-v','purple-v','pink-v']
     valid_colour = False
     for available in available_colours:
         if colour == available:valid_colour = True
-    if not valid_colour:print(f"\n  > FADE ERROR! - Invalid colour: {colour} | Available colours: {', '.join(available_colours)}");return
+    if not valid_colour:return clr(f"\n  > FADE ERROR! - Invalid colour: {colour} | Available colours: {', '.join(available_colours)}") + Style.RESET_ALL
         
     faded = ""
+    if len(text.splitlines()) > 1:multi_line = True
+    else:multi_line = False
 
     if colour == "black":
         for line in text.splitlines():
-            red = 0; green = 0; blue = 0
+            R = 0; G = 0; B = 0
             for character in line:
-                red += 3; green += 3; blue += 3
-                if red > 255 and green > 255 and blue > 255:red = 255; green = 255; blue = 255
-                faded += (f"\033[38;2;{red};{green};{blue}m{character}\033[0m")
-            faded += "\n"
+                R += 3; G += 3; B += 3
+                if R > 255 and G > 255 and B > 255:R = 255; G = 255; B = 255
+                faded += f"\033[38;2;{R};{G};{B}m{character}\033[0m"
+            if multi_line:faded += "\n"
             
     elif colour == "red":
         for line in text.splitlines():
-            green = 250
+            G = 250
             for character in line:
-                green -= 5
-                if green < 0:green = 0
-                faded += (f"\033[38;2;255;{green};0m{character}\033[0m")
-            faded += "\n"
+                G -= 5
+                if G < 0:G = 0
+                faded += f"\033[38;2;255;{G};0m{character}\033[0m"
+            if multi_line:faded += "\n"
             
     elif colour == "green":
         for line in text.splitlines():
-            red = 0
+            R = 0
             for character in line:
-                if not red > 200:red += 3
-                faded += (f"\033[38;2;{red};255;0m{character}\033[0m")
-            faded += "\n"
+                if not R > 200:R += 3
+                faded += f"\033[38;2;{R};255;0m{character}\033[0m"
+            if multi_line:faded += "\n"
             
     elif colour == "cyan":
         for line in text.splitlines():
-            blue = 100
+            B = 100
             for character in line:
-                blue += 2
-                if blue > 255:blue = 255
-                faded += (f"\033[38;2;0;255;{blue}m{character}\033[0m")
-            faded += "\n"
+                B += 2
+                if B > 255:B = 255
+                faded += f"\033[38;2;0;255;{B}m{character}\033[0m"
+            if multi_line:faded += "\n"
 
     elif colour == "blue":
         for line in text.splitlines():
-            green = 0
+            G = 0
             for character in line:
-                green += 3
-                if green > 255:green = 255
-                faded += (f"\033[38;2;0;{green};255m{character}\033[0m")
-            faded += "\n"
+                G += 3
+                if G > 255:G = 255
+                faded += f"\033[38;2;0;{G};255m{character}\033[0m"
+            if multi_line:faded += "\n"
         
     elif colour == "purple":
         for line in text.splitlines():
-            red = 35
+            R = 35
             for character in line:
-                red += 3
-                if red > 255:red = 255
-                faded += (f"\033[38;2;{red};0;220m{character}\033[0m")
-            faded += "\n"
+                R += 3
+                if R > 255:R = 255
+                faded += f"\033[38;2;{R};0;220m{character}\033[0m"
+            if multi_line:faded += "\n"
 
     elif colour == "black-v":
-        red = 0; green = 0; blue = 0
+        R = 0; G = 0; B = 0
         for line in text.splitlines():
-            faded += (f"\033[38;2;{red};{green};{blue}m{line}\033[0m\n")
-            if not red == 255 and not green == 255 and not blue == 255:
-                red += 20; green += 20; blue += 20
-                if red > 255 and green > 255 and blue > 255:red = 255; green = 255; blue = 255
+            faded += (f"\033[38;2;{R};{G};{B}m{line}\033[0m")
+            if not R == 255 and not G == 255 and not B == 255:
+                R += 20; G += 20; B += 20
+                if R > 255 and G > 255 and B > 255:R = 255; G = 255; B = 255
+            if multi_line:faded += "\n"
                     
     elif colour == "red-v":
-        green = 250
+        G = 250
         for line in text.splitlines():
-            faded += (f"\033[38;2;255;{green};0m{line}\033[0m\n")
-            if not green == 0:
-                green -= 25
-                if green < 0:
-                    green = 0
+            faded += f"\033[38;2;255;{G};0m{line}\033[0m"
+            if not G == 0:
+                G -= 25
+                if G < 0:
+                    G = 0
+            if multi_line:faded += "\n"
 
     elif colour == "green-v":
-        red = 0
+        R = 0
         for line in text.splitlines():
-            faded += (f"\033[38;2;{red};255;0m{line}\033[0m\n")
-            if not red > 200:red += 30
+            faded += f"\033[38;2;{R};255;0m{line}\033[0m"
+            if not R > 200:R += 30
+            if multi_line:faded += "\n"
         
     elif colour == "cyan-v":
-        blue = 100
+        B = 100
         for line in text.splitlines():
-            faded += (f"\033[38;2;0;255;{blue}m{line}\033[0m\n")
-            if not blue == 255:
-                blue += 15
-                if blue > 255:blue = 255
+            faded += f"\033[38;2;0;255;{B}m{line}\033[0m"
+            if not B == 255:
+                B += 15
+                if B > 255:B = 255
+            if multi_line:faded += "\n"
         
     elif colour == "blue-v":
-        green = 10
+        G = 10
         for line in text.splitlines():
-            faded += (f"\033[38;2;0;{green};255m{line}\033[0m\n")
-            if not green == 255:
-                green += 15
-                if green > 255:green = 255
+            faded += f"\033[38;2;0;{G};255m{line}\033[0m"
+            if not G == 255:
+                G += 15
+                if G > 255:G = 255
+            if multi_line:faded += "\n"
         
     elif colour == "purple-v":
-        red = 40
+        R = 40
         for line in text.splitlines():
-            faded += (f"\033[38;2;{red};0;220m{line}\033[0m\n")
-            if not red == 255:
-                red += 15
-                if red > 255:red = 255
+            faded += f"\033[38;2;{R};0;220m{line}\033[0m"
+            if not R == 255:
+                R += 15
+                if R > 255:R = 255
+            if multi_line:faded += "\n"
         
     elif colour == "pink-v":
-        blue = 255
+        B = 255
         for line in text.splitlines():
-            faded += (f"\033[38;2;255;0;{blue}m{line}\033[0m\n")
-            if not blue == 0:
-                blue -= 20
-                if blue < 0:blue = 0
+            faded += f"\033[38;2;255;0;{B}m{line}\033[0m"
+            if not B == 0:
+                B -= 20
+                if B < 0:B = 0
+            if multi_line:faded += "\n"
 
     elif colour == "random":
         for line in text.splitlines():
             for character in line:
-                faded += (f"\033[38;2;{random.randint(0,255)};{random.randint(0,255)};{random.randint(0,255)}m{character}\033[0m")
-            faded += "\n"
-        
-    else:print(clr(f"\n  > FADE ERROR! - [{colour}] is not supported yet!",2))
+                R, G, B = random.randint(0,255), random.randint(0,255), random.randint(0,255)
+                faded += f"\033[38;2;{R};{G};{B}m{character}\033[0m"
+            if multi_line:faded += "\n"
+    
+    else:return clr(f"\n  > FADE ERROR! - [{colour}] is not supported yet!",2) + Style.RESET_ALL
+    
+    if multi_line:faded = faded[:-1]
     return faded
