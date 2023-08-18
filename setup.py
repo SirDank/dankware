41d1bb1c8a700f3252615478db53127908e15f80
SirDank <52797753+SirDank@users.noreply.github.com>
2023-08-18 14:41:51 +0530

    3.4.2
    remove dankware_banner()

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
 
     license = "MIT",
     name = "dankware",
-    version = "3.4.1",
+    version = "3.4.2",
     author = "SirDank",
     
     author_email = "SirDankenstein@protonmail.com",
